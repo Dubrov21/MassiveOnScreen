df7afae8eeeb67db864264bcf7b95dd582d98214
Dubrov21 <vald5red@yandex.ru>
2023-10-06 20:25:41 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//Написать программу, которая задаёт массив из 8-ми элементов и выводит их на экран
+
+using System;
+
+namespace MassiveOnScreen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Write("[ ");
+
+            int[] numbers = { 2, 3, 4, 7, 9, 5, 12, 20 };
+
+            foreach(int i in numbers)
+            {
+                Console.Write($"{i}, ");
+            }
+            Console.Write("]");
+        }
+    }
+}
